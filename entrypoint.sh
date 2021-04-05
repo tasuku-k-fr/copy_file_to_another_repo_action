e1a21a909f9db2abf0e7cc9b49b93c2fd2b373f9
Tasuku Kuroiwa <tasuku-kuroiwa@c-fo.com>
2021-04-05 13:35:53 +0900

    Changed how variables are called literals

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,13 +3,13 @@
 set -e
 set -x
 
-if [ -z "$INPUT_SOURCE_FILE" ]
+if [ -z ${INPUT_SOURCE_FILE} ]
 then
   echo "Source file must be defined"
   return -1
 fi
 
-if [ -z "$INPUT_DESTINATION_BRANCH" ]
+if [ -z ${INPUT_DESTINATION_BRANCH} ]
 then
   INPUT_DESTINATION_BRANCH=main
 fi
@@ -24,7 +24,7 @@ git clone --single-branch --branch $INPUT_DESTINATION_BRANCH "https://x-access-t
 
 echo "Copying contents to git repo"
 mkdir -p $CLONE_DIR/$INPUT_DESTINATION_FOLDER
-cp -R "$INPUT_SOURCE_FILE" "$CLONE_DIR/$INPUT_DESTINATION_FOLDER"
+cp -R ${INPUT_SOURCE_FILE} "$CLONE_DIR/$INPUT_DESTINATION_FOLDER"
 cd "$CLONE_DIR"
 
 if [ ! -z "$INPUT_DESTINATION_BRANCH_CREATE" ]

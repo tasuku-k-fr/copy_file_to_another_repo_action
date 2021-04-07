45b890fdb0df74d0aaeeaa16ec497f24163b2e58
Tasuku Kuroiwa <tasuku-kuroiwa@c-fo.com>
2021-04-07 12:24:51 +0900

    wip

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,7 +28,7 @@ git config --global user.email "$INPUT_USER_EMAIL"
 git config --global user.name "$INPUT_USER_NAME"
 git clone --single-branch --branch $INPUT_DESTINATION_BRANCH "https://x-access-token:$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
 
-echo "Copying contents to git repo all file"
+echo "Copying contents to git repo all file escep"
 mkdir -p $CLONE_DIR/$INPUT_DESTINATION_FOLDER
 cp -R $INPUT_SOURCE_DIR_FILES\/\* "$CLONE_DIR/$INPUT_DESTINATION_FOLDER"
 cd "$CLONE_DIR"

2b55496a89f730b43261ff244d263f8e69c09ac4
Tasuku Kuroiwa <tasuku-kuroiwa@c-fo.com>
2021-04-30 08:26:46 +0900

    fix timing of remove

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,14 +21,16 @@ echo "Cloning destination git repository"
 git config --global user.email "$INPUT_USER_EMAIL"
 git config --global user.name "$INPUT_USER_NAME"
 git clone --single-branch --branch $INPUT_DESTINATION_BRANCH "https://x-access-token:$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
+# this line is remove command of gastbyjs build hash files.
+APP_ROOT_DIR = $CLONE_DIR/$INPUT_DESTINATION_FOLDER$INPUT_TARGET_DIR_NAME
+REMOVE_FILES = "$APP_ROOT_DIR/*.map $APP_ROOT_DIR/*.js $APP_ROOT_DIR/*.LICENSE.txt $APP_ROOT_DIR/*.css"
+rm -f $REMOVE_FILES
 
 echo "Copying contents to git repo all file escep"
 mkdir -p $CLONE_DIR/$INPUT_DESTINATION_FOLDER
 if [ "$INPUT_TARGET_DIR_NAME" ]
 then 
   mv $INPUT_SOURCE_FILE/ $INPUT_TARGET_DIR_NAME/
-  # this line is remove command of gastbyjs build hash files.
-  rm -f "$CLONE_DIR/$INPUT_DESTINATION_FOLDER/$INPUT_TARGET_DIR_NAME*.map" "$CLONE_DIR/$INPUT_DESTINATION_FOLDER/$INPUT_TARGET_DIR_NAME/*.js" "$CLONE_DIR/$INPUT_DESTINATION_FOLDER/$INPUT_TARGET_DIR_NAME/*.LICENSE.txt *.css"
   cp -R $INPUT_TARGET_DIR_NAME "$CLONE_DIR/$INPUT_DESTINATION_FOLDER"
 else
   cp -R $INPUT_SOURCE_FILE "$CLONE_DIR/$INPUT_DESTINATION_FOLDER"

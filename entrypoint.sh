a0657e4bab75916c436f17f5c21eae19a6bf30a4
Tasuku Kuroiwa <tasuku-kuroiwa@c-fo.com>
2021-04-30 08:32:14 +0900

    variable space remove

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,8 +22,8 @@ git config --global user.email "$INPUT_USER_EMAIL"
 git config --global user.name "$INPUT_USER_NAME"
 git clone --single-branch --branch $INPUT_DESTINATION_BRANCH "https://x-access-token:$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
 # this line is remove command of gastbyjs build hash files.
-APP_ROOT_DIR = $CLONE_DIR/$INPUT_DESTINATION_FOLDER$INPUT_TARGET_DIR_NAME
-REMOVE_FILES = "$APP_ROOT_DIR/*.map $APP_ROOT_DIR/*.js $APP_ROOT_DIR/*.LICENSE.txt $APP_ROOT_DIR/*.css"
+APP_ROOT_DIR=$CLONE_DIR/$INPUT_DESTINATION_FOLDER$INPUT_TARGET_DIR_NAME
+REMOVE_FILES="$APP_ROOT_DIR/*.map $APP_ROOT_DIR/*.js $APP_ROOT_DIR/*.LICENSE.txt $APP_ROOT_DIR/*.css"
 rm -f $REMOVE_FILES
 
 echo "Copying contents to git repo all file escep"
